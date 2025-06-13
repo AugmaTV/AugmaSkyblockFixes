04a6bb3726c5dc10ff22ef44d4b1c991e10427c4
Augma <cloumic98@gmail.com>
2025-06-13 21:33:48 +0200

    change the way to detect the skyblock menu item

--- src/client/java/fr/augma/augmaskyblockfix/client/mixin/AbstractContainerScreenMixin.java
+++ src/client/java/fr/augma/augmaskyblockfix/client/mixin/AbstractContainerScreenMixin.java
@@ -3,6 +3,7 @@ package fr.augma.augmaskyblockfix.client.mixin;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.InventoryMenu;
@@ -13,6 +14,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin<T extends AbstractContainerMenu> extends Screen implements MenuAccess<T> {
@@ -28,9 +31,12 @@ public abstract class AbstractContainerScreenMixin<T extends AbstractContainerMe
     public void removed(CallbackInfo ci) {
         if (menu instanceof InventoryMenu) {
             final InventoryMenu inventoryMenu = (InventoryMenu) menu;
-
-            if (inventoryMenu.getCarried() != ItemStack.EMPTY && inventoryMenu.getCarried().getItem() == Items.NETHER_STAR && inventoryMenu.getCarried().getComponents().size() == 14) {
-                inventoryMenu.setCarried(ItemStack.EMPTY);
+            if (inventoryMenu.getCarried() != ItemStack.EMPTY && inventoryMenu.getCarried().getItem() == Items.NETHER_STAR) {
+                final ItemStack itemStack = inventoryMenu.getCarried();
+                final Optional<String> optSkyblockId = itemStack.getComponents().get(DataComponents.CUSTOM_DATA).copyTag().getString("id");
+                if (optSkyblockId.isPresent() && optSkyblockId.get().equalsIgnoreCase("SKYBLOCK_MENU")) {
+                    inventoryMenu.setCarried(ItemStack.EMPTY);
+                }
             }
         }
     }
