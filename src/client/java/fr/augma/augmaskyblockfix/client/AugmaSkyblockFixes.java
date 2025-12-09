cfa7941ed18e1c7623e71eff48da4072210c6a7a
Augma <cloumic98@gmail.com>
2025-12-09 21:11:55 +0100

    add config command

--- src/client/java/fr/augma/augmaskyblockfix/client/AugmaSkyblockFixes.java
+++ src/client/java/fr/augma/augmaskyblockfix/client/AugmaSkyblockFixes.java
@@ -1,13 +1,18 @@
 package fr.augma.augmaskyblockfix.client;
 
+import fr.augma.augmaskyblockfix.client.command.ConfigCommand;
 import fr.augma.augmaskyblockfix.client.config.ModConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class AugmaSkyblockFixes implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         ModConfig.HANDLER.load();
+        
+        // Register client commands
+        ClientCommandRegistrationCallback.EVENT.register(ConfigCommand::register);
     }
 
 }
