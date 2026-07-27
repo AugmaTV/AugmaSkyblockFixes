9f27f1c368c95f02c887033919719dfffbd38f05
Théo Thierry <cloumic98@gmail.com>
2026-07-27 13:24:55 +0200

    fix: refresh chunks when toggling centered plants

--- src/client/java/fr/augma/augmaskyblockfix/client/config/MiscellaneousConfig.java
+++ src/client/java/fr/augma/augmaskyblockfix/client/config/MiscellaneousConfig.java
@@ -3,6 +3,7 @@ package fr.augma.augmaskyblockfix.client.config;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import io.github.notenoughupdates.moulconfig.observer.Property;
 import lombok.Getter;
 
 @Getter
@@ -11,6 +12,10 @@ public class MiscellaneousConfig {
 	@Expose
 	@ConfigOption(name = "Centered plants", desc = "Remove the random offset applied to plants so they stay centered on their block")
 	@ConfigEditorBoolean
-	public boolean centeredPlants = false;
+	public Property<Boolean> centeredPlants = Property.of(false);
+
+	public boolean isCenteredPlants() {
+		return this.centeredPlants.get();
+	}
 
 }
