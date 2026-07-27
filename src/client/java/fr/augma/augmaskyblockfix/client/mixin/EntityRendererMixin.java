300f749f23600b69d708ed42de6c5a288ecff7ee
Théo Thierry <cloumic98@gmail.com>
2026-07-27 21:25:29 +0200

    fix: use vanilla line width for entity hitbox

--- src/client/java/fr/augma/augmaskyblockfix/client/mixin/EntityRendererMixin.java
+++ src/client/java/fr/augma/augmaskyblockfix/client/mixin/EntityRendererMixin.java
@@ -3,6 +3,7 @@ package fr.augma.augmaskyblockfix.client.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import fr.augma.augmaskyblockfix.client.config.EntityEntryConfig;
 import fr.augma.augmaskyblockfix.client.config.ModConfig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
@@ -28,10 +29,11 @@ public abstract class EntityRendererMixin {
 
         final AABB relativeBox = new AABB(-state.boundingBoxWidth / 2, 0, -state.boundingBoxWidth / 2, state.boundingBoxWidth / 2, state.boundingBoxHeight, state.boundingBoxWidth / 2);
         final int packedColor = entry.getHitboxRgb();
+        final float lineWidth = Minecraft.getInstance().gameRenderer.getGameRenderState().windowRenderState.appropriateLineWidth;
         submitNodeCollector.submitCustomGeometry(poseStack, RenderTypes.lines(), (pose, vertexConsumer) -> Shapes.create(relativeBox).forAllEdges((x1, y1, z1, x2, y2, z2) -> {
             final Vector3f normal = new Vector3f((float) (x2 - x1), (float) (y2 - y1), (float) (z2 - z1)).normalize();
-            vertexConsumer.addVertex(pose, (float) x1, (float) y1, (float) z1).setColor(packedColor).setNormal(pose, normal).setLineWidth(2.5F);
-            vertexConsumer.addVertex(pose, (float) x2, (float) y2, (float) z2).setColor(packedColor).setNormal(pose, normal).setLineWidth(2.5F);
+            vertexConsumer.addVertex(pose, (float) x1, (float) y1, (float) z1).setColor(packedColor).setNormal(pose, normal).setLineWidth(lineWidth);
+            vertexConsumer.addVertex(pose, (float) x2, (float) y2, (float) z2).setColor(packedColor).setNormal(pose, normal).setLineWidth(lineWidth);
         }));
     }
 
