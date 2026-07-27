f95fa73b95e5fd701ee16fe0e62e91a452b00c81
Théo Thierry <cloumic98@gmail.com>
2026-07-27 20:18:05 +0200

    feat: unlimited per-entity slots via dynamic config category

--- src/client/java/fr/augma/augmaskyblockfix/client/config/ModConfigScreen.java
+++ src/client/java/fr/augma/augmaskyblockfix/client/config/ModConfigScreen.java
@@ -2,6 +2,7 @@ package fr.augma.augmaskyblockfix.client.config;
 
 import io.github.notenoughupdates.moulconfig.gui.GuiContext;
 import io.github.notenoughupdates.moulconfig.gui.GuiElementComponent;
+import io.github.notenoughupdates.moulconfig.managed.ManagedConfig;
 import io.github.notenoughupdates.moulconfig.platform.MoulConfigScreenComponent;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -9,11 +10,15 @@ import net.minecraft.network.chat.Component;
 public class ModConfigScreen {
 
 	public static Screen create(final Screen parent) {
-		return new MoulConfigScreenComponent(Component.literal("AugmaSkyblockFixes Configuration"), new GuiContext(new GuiElementComponent(ModConfig.managed().getEditor())), parent) {
+		final ManagedConfig<ModConfig> managed = ModConfig.managed();
+		managed.rebuildConfigProcessor();
+		DynamicEntityCategory.install(managed.getProcessor(), managed.getInstance().getEntities());
+
+		return new MoulConfigScreenComponent(Component.literal("AugmaSkyblockFixes Configuration"), new GuiContext(new GuiElementComponent(managed.getEditor())), parent) {
 			@Override
 			public void removed() {
 				super.removed();
-				ModConfig.managed().saveToFile();
+				managed.saveToFile();
 			}
 		};
 	}
